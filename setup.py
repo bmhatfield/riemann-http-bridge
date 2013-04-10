18f5b44907fb3762b907b741d278a775c05348ed
Brian Hatfield <bhatfield@brightcove.com>
2013-04-10 12:38:15 -0400

    Add monitoring-bridge support files

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+version = "0.1.0"
+
+setup(name="riemann-http-bridge",
+      version=version,
+      description="Python agent for proxying HTTP requests to Riemann",
+      author="Brian Hatfield",
+      author_email="bmhatfield@gmail.com",
+      url="https://github.com/bmhatfield/riemann-http-bridge",
+      data_files=[('/etc/init/', ["init/ubuntu/riemann-http-bridge.conf"])],
+      scripts=["riemann-http-bridge.py"]
+    )
